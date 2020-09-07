081e37285e71c6321365258cfd7fc0f163c3f1ae
Alan Rodriguez <alan.nicolas.rodriguez@gmail.com>
2020-09-07 16:37:07 -0300

    header con logo pero sin donaciones

--- ManosALaObraFrontend/src/app/header/header.component.ts
+++ ManosALaObraFrontend/src/app/header/header.component.ts
@@ -1,23 +1,15 @@
-/** REVISAR Y MODIFICAR!**/
-
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { AppComponent } from '../app.component';
-import { DataService } from '../data.service';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-
 export class HeaderComponent implements OnInit {
 
-    constructor(private api: ApiService, private http: HttpClient, public appComp: AppComponent, public data: DataService){
+  constructor() { }
 
-    }
+  ngOnInit() {
+  }
 
-    ngOnInit(){
-
-    }
 }
