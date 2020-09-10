63430a6e63082a88fa0bdd722a980d1c6fca0993
Alan Rodriguez <alan.nicolas.rodriguez@gmail.com>
2020-09-10 19:05:32 -0300

    muestra formulario, darle un lindo formato

--- ManosALaObraFrontend/src/app/app-routing.module.ts
+++ ManosALaObraFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {FormComponent} from './form/form.component';
 
 const routes: Routes = [
     {path: "", pathMatch: "full", redirectTo: "home"},
-    {path: "home", component: HomeComponent}
+    {path: "home", component: HomeComponent},
+    {path: "form", component: FormComponent}
 ];
 
 @NgModule({
