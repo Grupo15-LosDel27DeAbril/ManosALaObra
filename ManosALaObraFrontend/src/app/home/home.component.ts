39fbc9ff2b658f825a3231d4b3309f1525906a47
Alan Rodriguez <alan.nicolas.rodriguez@gmail.com>
2020-11-07 08:42:52 -0300

    migrando de component

--- ManosALaObraFrontend/src/app/home/home.component.ts
+++ ManosALaObraFrontend/src/app/home/home.component.ts
@@ -10,109 +10,76 @@ import { StateComponent } from '../state/state.component';
 //import { registerLocaleData } from '@angular/common';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
 
-    selectedDonacion: Producto;
-    solicitoDonacion: Boolean = false;
-    nombreUsuario: string;
-    count: number = 1;
-    nombreProducto: string;
-    productos: Producto[] = [];
-    registros: Registro[] = [];
-    fileData: File;
-  previewUrl: any = null;
-  fileUploadProgress: string = null;
-  uploadedFilePath: string = null;
+  selectedDonacion: Producto;
+  solicitoDonacion: Boolean = false;
+  nombreUsuario: string;
+  count: number = 1;
+  nombreProducto: string;
+  productos: Producto[] = [];
+  registros: Registro[] = [];
 
 
-    constructor(private api: ApiService, private http: HttpClient, public appComp: AppComponent, public data: DataService, private route: Router) {
+  constructor(private api: ApiService, private http: HttpClient, public appComp: AppComponent, public data: DataService, private route: Router) {
 
-    }
+  }
 
-    ngOnInit() {
-        this.api.getDonacionesAPI$().subscribe(resp => { this.productos = resp; this.data.productos = resp });
-        this.api.getRegistrosAPI$().subscribe(resp => { this.registros = resp; this.data.registros = resp });
-        console.log("Save button is clicked!", this.data.productos);
-        console.log("El usuario logueado es: ", this.data.userData);
-        console.log("Los registros son: ", this.data.registros)
-    }
+  ngOnInit() {
+    this.api.getDonacionesAPI$().subscribe(resp => { this.productos = resp; this.data.productos = resp });
+    this.api.getRegistrosAPI$().subscribe(resp => { this.registros = resp; this.data.registros = resp });
+    console.log("Save button is clicked!", this.data.productos);
+    console.log("El usuario logueado es: ", this.data.userData);
+    console.log("Los registros son: ", this.data.registros)
+  }
 
-    public ubicacionEnMapa(producto) {
-        this.data.lat = producto.latitude;
-        this.data.lng = producto.longitude;
-        this.data.lugar = producto.lugar;
-        this.route.navigateByUrl('map');
-    }
+  public ubicacionEnMapa(producto) {
+    this.data.lat = producto.latitude;
+    this.data.lng = producto.longitude;
+    this.data.lugar = producto.lugar;
+    this.route.navigateByUrl('map');
+  }
 
-    public completarFormulario(producto) {
-        console.log("El email del que hizo esta donacion es: ", producto.emailDonante);
-        this.appComp.solicitarLaDonacion(producto.emailDonante);
-        this.appComp.agregarMailSolicitante(producto.id);
+  public completarFormulario(producto) {
+    console.log("El email del que hizo esta donacion es: ", producto.emailDonante);
+    this.appComp.solicitarLaDonacion(producto.emailDonante);
+    this.appComp.agregarMailSolicitante(producto.id);
 
-        //console.log("Save button is clicked!", this.data.productos);
-        this.route.navigateByUrl('form');
-    }
+    //console.log("Save button is clicked!", this.data.productos);
+    this.route.navigateByUrl('form');
+  }
 
-    public contieneMail(producto: Producto, mail: string) {
-        var res: Boolean = false;
-        for (const data of producto.emailsSolicitantes) {
-            if (data.mail == mail) {
-                res = true;
-            }
-        }
-        return res;
-    }
-
-    public condition(producto) {
-        var res: Boolean = false;
-        for (const data of producto.emailsSolicitantes) {
-            res = res || data.mail == this.data.userData.email;
-            res = res || producto.estado == "Finalizado";
-            res = res || producto.estado == "Entregado";
-        }
-        return res;
-    }
-
-    public esAdministrador(producto) {
-        return producto.emailDonante == this.data.userData.nombreUsuario;
-    }
-
-    public verEstado(producto) {
-        this.data.emailsActuales = producto.emailsSolicitantes;
-        this.route.navigateByUrl('state');
-    }
-    preview() {
-        // Show preview 
-        var mimeType = this.fileData.type;
-        if (mimeType.match(/image\/*/) == null) {
-          return;
-        }
-      
-        var reader = new FileReader();      
-        reader.readAsDataURL(this.fileData); 
-        reader.onload = (_event) => { 
-          this.previewUrl = reader.result; 
-        }
+  public contieneMail(producto: Producto, mail: string) {
+    var res: Boolean = false;
+    for (const data of producto.emailsSolicitantes) {
+      if (data.mail == mail) {
+        res = true;
       }
-      onSubmit() {
-        var urlLocal = 'http://localhost:8080/api/';
-      
-        var reader  = new FileReader();
-        const formData = new FormData();
-          formData.append('file', this.fileData);
-          this.http.post(urlLocal+'/upload', formData)
-            .subscribe(res => {
-              console.log(res);
-              reader.readAsDataURL(this.fileData);
-              this.uploadedFilePath = reader.result.toString();
-              //this.uploadedFilePath = res.data.filePath;//direccion que quiero capturar
-              alert('SUCCESS !!');
-            })
-      }
+    }
+    return res;
+  }
 
+  public condition(producto) {
+    var res: Boolean = false;
+    for (const data of producto.emailsSolicitantes) {
+      res = res || data.mail == this.data.userData.email;
+      res = res || producto.estado == "Finalizado";
+      res = res || producto.estado == "Entregado";
+    }
+    return res;
+  }
+
+  public esAdministrador(producto) {
+    return producto.emailDonante == this.data.userData.nombreUsuario;
+  }
+
+  public verEstado(producto) {
+    this.data.emailsActuales = producto.emailsSolicitantes;
+    this.route.navigateByUrl('state');
+  }
 }
--- ManosALaObraFrontend/src/app/http-client/http-client.component.ts
+++ ManosALaObraFrontend/src/app/http-client/http-client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams,HttpEventType } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-http-client',
@@ -6,10 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./http-client.component.css']
 })
 export class HttpClientComponent implements OnInit {
+  fileData: File;
+  previewUrl: any = null;
+  fileUploadProgress: string = null;
+  uploadedFilePath: string = null;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+  preview() {
+    // Show preview 
+    var mimeType = this.fileData.type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.readAsDataURL(this.fileData);
+    reader.onload = (_event) => {
+      this.previewUrl = reader.result;
+    }
+  }
+  /*
+  onSubmit() {
+    var urlLocal = 'http://localhost:4200/api/';
+
+    var reader = new FileReader();
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http.post(urlLocal + 'fileUpload', formData)
+      .subscribe(res => {
+        console.log(res);
+        reader.readAsDataURL(this.fileData);
+        this.uploadedFilePath = reader.result.toString();
+        //this.uploadedFilePath = res.data.filePath;//direccion que quiero capturar
+        alert('SUCCESS !!');
+      })
+  }*/
+
+   
+onSubmit() {
+  const formData = new FormData();
+  formData.append('files', this.fileData);
+  var urlLocal = 'http://localhost:4200/api/';
+ 
+  this.fileUploadProgress = '0%';
+
+  this.http.post(urlLocal+'fileUpload', formData, {
+    reportProgress: true,
+    observe: 'events'   
+  })
+  .subscribe(events => {
+    if(events.type === HttpEventType.UploadProgress) {
+      this.fileUploadProgress = Math.round(events.loaded / events.total * 100) + '%';
+      console.log(this.fileUploadProgress);
+    } else if(events.type === HttpEventType.Response) {
+      this.fileUploadProgress = ''; 
+      console.log(events.body);          
+      alert('SUCCESS !!');
+    }
+       
+  }) 
+}
+
+
 
 }
